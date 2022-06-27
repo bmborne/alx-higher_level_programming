60b942cb709e5bbfc6f5096961b5ca9137c62104
bmborne <boniphacemkindi@gmail.commit>
2022-06-27 12:31:45 -0700

    Hello World

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,12 +5,12 @@
 
 /**
  * struct listint_s - singly linked list
- * @n : interger
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *
  */
-
 typedef struct listint_s
 {
 int n;
@@ -22,4 +22,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
