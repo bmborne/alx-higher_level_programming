e9fd7600d6e66462f331344800f025a6bd33c162
bmborne <boniphacemkindi@gmail.commit>
2022-06-30 09:58:48 -0700

    Hello World

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-
-a = 1
-b = 2
-
-# <a value> + <b value> = <add(a, b) value>
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ = "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    # <a value> + <b value> = <add(a, b) value>
+    print("{} + {} = {}".format(a, b, add(a, b)))
